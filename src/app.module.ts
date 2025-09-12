0ee836d3c9f00de61b2da5c38c1628dd1fad24ad
Teo Lunas <lunasteobaldo@gmail.com>
2025-09-12 16:55:06 -0300

    Se crea Modulo para el manejo de clientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/app.config';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration]
-    })
+    }),
+    ClientesModule
   ],
   controllers: [AppController],
   providers: [AppService],
