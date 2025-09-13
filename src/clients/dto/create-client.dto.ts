99f0ab9c36d18fc0f8973fa9018c05cd5ae310c9
Teo Lunas <lunasteobaldo@gmail.com>
2025-09-12 21:59:50 -0300

    Se crea:
    - Archivo DTO para recibir datos para crear un nuevo cliente

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,1 +1,28 @@
-export class CreateClientDto {}
+import { IsBoolean, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateClientDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    phone: string;
+
+    @IsString()
+    @IsOptional()
+    addres: string;
+
+    @IsString()
+    @IsOptional()
+    legalRepresentative: string;
+
+    @IsBoolean()
+    @IsOptional()
+    active: boolean;
+}
