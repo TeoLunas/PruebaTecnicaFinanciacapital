b7bca2c65f78a139db0ab515f65d3b159e42eb95
Teo Lunas <lunasteobaldo@gmail.com>
2025-09-15 03:15:10 -0300

    Se agrega entidad de deudor para ser usado tanto en migraciones como en servicios

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Client } from './clients/entities/client.entity';
 import { Invoice } from './invoices/entities/invoice.entity';
+import { Debtor } from './debtors/entities/debtor.entity';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -10,7 +11,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASS || 'ClaveSecreta1',
     database: process.env.DB_NAME || 'PruebaTecnicaFinanciaCapital',
-    entities: [Client, Invoice],
+    entities: [Client, Invoice, Debtor],
     migrations: ['src/migrations/*.ts'],
     synchronize: false,
 });
--- src/debtors/debtors.module.ts
+++ src/debtors/debtors.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DebtorsService } from './debtors.service';
 import { DebtorsController } from './debtors.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Debtor } from './entities/debtor.entity';
 
 @Module({
   controllers: [DebtorsController],
   providers: [DebtorsService],
+  imports:[ 
+    TypeOrmModule.forFeature([Debtor])
+  ]
 })
 export class DebtorsModule {}
