9c5f29ef4763ff40c548c78a3e67958113ce5c7d
Teo Lunas <lunasteobaldo@gmail.com>
2025-09-15 03:22:07 -0300

    Se crea dto para crear deudores(debtors)

--- src/debtors/dto/create-debtor.dto.ts
+++ src/debtors/dto/create-debtor.dto.ts
@@ -1,1 +1,28 @@
-export class CreateDebtorDto {}
+import { IsBoolean, IsOptional, IsString } from "class-validator";
+
+export class CreateDebtorDto {
+    @IsString()
+    nombre: string;
+
+    @IsString()
+    rut: string;
+
+    @IsString()
+    giro: string;
+
+    @IsString()
+    direccion: string;
+
+    @IsString()
+    comuna: string;
+
+    @IsString()
+    ciudad: string;
+
+    @IsString()
+    contacto: string;
+
+    @IsBoolean()
+    @IsOptional()
+    active?: boolean;
+}
