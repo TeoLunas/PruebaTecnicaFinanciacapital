a33eb25d5b19b1800f0411e3ead49be4f24facd2
Teo Lunas <lunasteobaldo@gmail.com>
2025-09-15 01:37:52 -0300

    Se crea entidad para valores de tabla de facturas(invoice)

--- src/invoices/entities/invoice.entity.ts
+++ src/invoices/entities/invoice.entity.ts
@@ -1,1 +1,71 @@
-export class Invoice {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('invoices')
+export class Invoice {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('integer')
+    numeroFactura: number;
+    @Column('text')
+    fechaEmision: string;
+
+    // Datos de la empresa que emite la facutura osea mi cliente. 
+    @Column('text')
+    nombreEmpresa: string;
+    @Column('text')
+    giroEmpresa: string;
+    @Column('text')
+    emailEmpresa: string;
+    @Column('text')
+    direccionEmpresa: string;
+    @Column('text')
+    telefono: string;
+
+    // Datos del cliente en este caso seria el deudor.
+    @Column('text')
+    rutCliente:  string;
+    @Column('text')
+    giroCliente: string;
+    @Column('text')
+    direccionCliente: string;
+    @Column('text')
+    comunaCliente: string;
+    @Column('text')
+    ciudadCliente: string;
+    @Column('text')
+    contactoCliente: string;
+    @Column('text')
+    tipoCompraCliente: string;
+    @Column('text')
+    tipoVentaCliente: string;
+
+    // Detalle de productos en venta;
+    @Column('text')
+    codigoProducto?: string;
+    @Column('text')
+    descripcionProducto: string;
+    @Column('text')
+    cantidad: string;
+    @Column('text')
+    precio: number;
+    @Column('text')
+    impuestoAdic: number;
+    @Column('text')
+    desc?: string;
+    @Column('bigint')
+    valor: number;
+    @Column('text')
+    formaPago: string;
+
+    // Totales
+    @Column('bigint')
+    montoNeto: number;
+    @Column('bigint')
+    iva: number;
+    @Column('bigint')
+    impuestoAdicional: number;
+    @Column('bigint')
+    total: number;
+
+}
