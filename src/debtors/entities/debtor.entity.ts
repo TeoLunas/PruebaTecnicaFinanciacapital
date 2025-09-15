934b5b14ca552efc335d497cb9db43fc8648fe75
Teo Lunas <lunasteobaldo@gmail.com>
2025-09-15 03:12:37 -0300

    Se crea entidad para manejar deudores

--- src/debtors/entities/debtor.entity.ts
+++ src/debtors/entities/debtor.entity.ts
@@ -1,1 +1,24 @@
-export class Debtor {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('debtors')
+export class Debtor {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column('text')
+    nombre: string;
+    @Column('text')
+    rut: string;
+    @Column('text')
+    giro: string;
+    @Column('text')
+    direccion: string;
+    @Column('text')
+    comuna: string;
+    @Column('text')
+    ciudad: string;
+    @Column('text')
+    contacto: string;
+    @Column('boolean', {default: true})
+    active: boolean;
+}
