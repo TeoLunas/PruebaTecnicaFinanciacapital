69319b9ee85b3f6681512bc760a744b2b45d577d
Teo Lunas <lunasteobaldo@gmail.com>
2025-09-15 04:04:24 -0300

    Se modifica campo para que rut sea unico

--- src/debtors/entities/debtor.entity.ts
+++ src/debtors/entities/debtor.entity.ts
@@ -5,20 +5,28 @@ export class Debtor {
 
     @PrimaryGeneratedColumn()
     id: number;
+
     @Column('text')
     nombre: string;
-    @Column('text')
+    
+    @Column('text', {unique: true})
     rut: string;
+    
     @Column('text')
     giro: string;
+    
     @Column('text')
     direccion: string;
+    
     @Column('text')
     comuna: string;
+    
     @Column('text')
     ciudad: string;
+    
     @Column('text')
     contacto: string;
+    
     @Column('boolean', {default: true})
-    active: boolean;
+    activo: boolean;
 }
